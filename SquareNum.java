f73b950a98abafb65c24f990d7a54ef05434ec69
Chandrashekar G <110971589+chandrashekarg6119@users.noreply.github.com>
2026-02-10 12:25:33 +0530

    Update SquareNum.java

--- SquareNum.java
+++ SquareNum.java
@@ -1,56 +1,26 @@
-package com.example;
-
-import java.util.Scanner;
-
-class SquareCheck {
-
-    // Function to find distance between two points
-    static int dist(int x1, int y1, int x2, int y2) {
-        return (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1);
+public class SquareNum{
+    static int distSq(int x1, int y1, int x2, int y2) {
+        return (x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2);
     }
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        int x1 = 20, y1 = 10;
+        int x2 = 10, y2 = 20;
+        int x3 = 20, y3 = 20;
+        int x4 = 10, y4 = 10;
 
-        // Input four points
-        System.out.print("Enter x1 y1: ");
-        int x1 = sc.nextInt();
-        int y1 = sc.nextInt();
+        int centerX = (x1 + x2 + x3 + x4) / 4;
+        int centerY = (y1 + y2 + y3 + y4) / 4;
 
-        System.out.print("Enter x2 y2: ");
-        int x2 = sc.nextInt();
-        int y2 = sc.nextInt();
+        int d1 = distSq(x1, y1, centerX, centerY);
+        int d2 = distSq(x2, y2, centerX, centerY);
+        int d3 = distSq(x3, y3, centerX, centerY);
+        int d4 = distSq(x4, y4, centerX, centerY);
 
-        System.out.print("Enter x3 y3: ");
-        int x3 = sc.nextInt();
-        int y3 = sc.nextInt();
-
-        System.out.print("Enter x4 y4: ");
-        int x4 = sc.nextInt();
-        int y4 = sc.nextInt();
-
-        // Calculate distances
-        int d1 = dist(x1, y1, x2, y2);
-        int d2 = dist(x1, y1, x3, y3);
-        int d3 = dist(x1, y1, x4, y4);
-        int d4 = dist(x2, y2, x3, y3);
-        int d5 = dist(x2, y2, x4, y4);
-        int d6 = dist(x3, y3, x4, y4);
-
-        // Square condition check
-        if (d1 > 0 &&
-                d1 == d2 &&
-                d2 == d3 &&
-                d4 == d5 &&
-                d1 == d4 &&
-                d6 == 2 * d1) {
-
+        if (d1 == d2 && d2 == d3 && d3 == d4)
             System.out.println("Yes");
-        } else {
+        else
             System.out.println("No");
-        }
-
-        sc.close();
     }
 }
