fc481351c4de59f951e23dfc1fd829c1c5e97a66
Chandrashekar G <110971589+chandrashekarg6119@users.noreply.github.com>
2026-03-24 16:34:19 +0530

    hospital-queue-system directory

--- hospital-queue-system/src/main/java/com/example/hospitalqueuesystem/repository/DoctorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.hospitalqueuesystem.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.hospitalqueuesystem.entity.Doctor;
-
-public interface DoctorRepository extends JpaRepository<Doctor,Long>{
-
-}
