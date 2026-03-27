fb11c6eed1f6acaad464e3f5491a9d038a67dc5f
Chandrashekar G <110971589+chandrashekarg6119@users.noreply.github.com>
2026-03-27 10:58:54 +0530

    Delete coding-platform directory

--- coding-platform/src/main/java/com/example/codingplatform/repository/ProblemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.codingplatform.repository;
-
-import com.example.codingplatform.entity.Problem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProblemRepository extends JpaRepository<Problem, Long> {
-}
--- coding-platform/src/main/java/com/example/codingplatform/repository/SubmissionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.codingplatform.repository;
-
-import com.example.codingplatform.entity.Submission;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubmissionRepository extends JpaRepository<Submission, Long> {
-}
